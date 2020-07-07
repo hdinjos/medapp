33e6bb15852cb4512a010c605e15d975111818a1
hdinjos <hdinjos@gmail.com>
2020-07-07 13:27:03 +0700

    import module mustache and use

--- server.js
+++ server.js
@@ -1,7 +1,25 @@
+//import module
 const express = require("express");
-const app = express();
+const mustacheExpress = require("mustache-express");
 
+//use module
+const app = express();
+const mustache = mustacheExpress();
+
+//setting up template engine
+mustache.cache = null;
+app.engine("mustache", mustache);
+app.set("view engine", "mustache");
+
+//setting up storage
 app.use(express.static("public"));
+
+//setting simple routing
+app.get("/meds", (req, res) => {
+    res.render("meds");
+});
+
+//app listen
 app.listen(5000, () => {
     console.log("server listen port 5000");
 });
