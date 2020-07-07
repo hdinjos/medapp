807098fe3937d216fb07a45e2252ba246b33a0d3
hdinjos <hdinjos@gmail.com>
2020-07-07 15:48:27 +0700

    use body-parser and make simple route to create data

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 //import module
 const express = require("express");
 const mustacheExpress = require("mustache-express");
+const bodyParser = require("body-parser");
 
 //use module
 const app = express();
@@ -14,11 +15,22 @@ app.set("view engine", "mustache");
 //setting up storage
 app.use(express.static("public"));
 
+//setting up data input to json
+app.use(bodyParser.urlencoded({ extended: false }));
+
 //setting simple routing
 app.get("/meds", (req, res) => {
     res.render("meds");
 });
 
+app.get("/add", (req, res) => {
+    res.render("med-form");
+});
+
+app.post("/meds/add", (req, res) => {
+    console.log("post ", req.body);
+    res.redirect("/meds");
+});
 //app listen
 app.listen(5000, () => {
     console.log("server listen port 5000");
